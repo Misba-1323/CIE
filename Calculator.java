1f0d68dce677b27e227acd6e2881bc0c102e5aaf
Misba-1323 <misbakousarrjamadar@gmail.com>
2025-04-15 09:52:24 +0530

    Save Calculator.java file

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+package cie;
+
+import java.util.Scanner;
+
+public class Calculator{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number : ");
+        int n1 = sc.nextInt();
+
+        System.out.println("Enter second number : ");
+        int n2 = sc.nextInt();
+
+        System.out.println("Addition = "+(n1+n2));
+
+        System.out.println("Subtraction = "+(n1-n2));
+
+        System.out.println("Multiplication = "+(n1*n2));
+
+        System.out.println("Division = "+(n1/n2));
+
+        sc.close();
+    }
+}
